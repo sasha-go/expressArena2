c1f4c3913052f47fa487c0108ae940ada18bdc01
sasha-go <sasha.lfg@gmail.com>
2020-03-21 14:57:24 -0700

    added some drills

--- app.js
+++ app.js
@@ -30,6 +30,14 @@ app.get('/echo', (req, res) => {
   res.send(responseText);
 });
 
+
+app.get('/hello', (req, res) => {
+  res
+		.status(204)
+		.end()
+});
+
+
 app.get('/queryViewer', (req, res) => {
 	console.log(req.query);
 	res.end();
@@ -57,6 +65,53 @@ app.get('/greetings', (req, res) => {
   //6. send the response 
   res.send(greeting);
 });
+
+
+
+app.get('/grade', (req, res) => {
+  // get the mark from the query
+  const { mark } = req.query;
+
+  // do some validation
+  if (!mark) {
+    // mark is required
+    return res
+      .status(400)
+      .send('Please provide a mark');
+  }
+
+  const numericMark = parseFloat(mark);
+  if (Number.isNaN(numericMark)) {
+    // mark must be a number
+    return res
+      .status(400)
+      .send('Mark must be a numeric value');
+  }
+
+  if (numericMark < 0 || numericMark > 100) {
+    // mark must be in range 0 to 100
+    return res
+      .status(400)
+      .send('Mark must be in range 0 to 100');
+  }
+
+  if (numericMark >= 90) {
+    return res.send('A');
+  }
+
+  if (numericMark >= 80) {
+    return res.send('B');
+  }
+
+  if (numericMark >= 70) {
+    return res.send('C');
+  }
+
+  res.send('F');
+});
+
+
+
 
 
 
